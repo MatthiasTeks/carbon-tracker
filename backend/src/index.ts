06e55c6a888ca1e1b8b2950a24d77e665efa426d
Matthias <matthias.vimbert@gmail.com>
2024-01-30 14:24:18 +0100

    test husky

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,5 +13,5 @@ buildSchema({
   await db.initialize();
   const server = new ApolloServer({ schema });
   const { url } = await startStandaloneServer(server, { listen: { port } });
-  console.info(`graphql server listening  on ${url}`);
+  console.info(`graphql server listeninggg  on ${url}`);
 });
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -110,7 +110,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Instantly deploy your Nextxx.js site to a shareable URL with Vercel.
           </p>
         </a>
       </div>
