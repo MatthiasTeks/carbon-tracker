1b1ab3ba71ced039c8ade592bbbc055805846457
Matthias VIMBERT <48927943+MatthiasTeks@users.noreply.github.com>
2024-01-31 12:21:35 +0100

    Matthias/lint prettier (#2)
    * basic setup lint, prettier, husky
    
    * test
    
    * fix project according to linter and prettier settings
    
    * test husky
    
    * test husky
    
    * linter/prettier/husky/codegen/callapi
    
    * linter/prettier/husky/codegen/callapi
    
    * test
    
    * final push

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,14 +1,14 @@
-import { DataSource } from "typeorm";
-import { Book } from "./entities/book";
+import { DataSource } from 'typeorm';
+import Book from './entities/book';
 
 export default new DataSource({
-    type: "postgres",
-    host: process.env.DB_HOST || "db",
-    port: parseInt(process.env.DB_PORT || "0") || 5432,
-    username: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASS || "postgres",
-    database: process.env.DB_NAME || "postgres",
-    entities: [Book],
-    synchronize: true,
-    logging: true,
+  type: 'postgres',
+  host: process.env.DB_HOST || 'db',
+  port: parseInt(process.env.DB_PORT || '0', 10) || 5432,
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASS || 'postgres',
+  database: process.env.DB_NAME || 'postgres',
+  entities: [Book],
+  synchronize: true,
+  logging: true,
 });
