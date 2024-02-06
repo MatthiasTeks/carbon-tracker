d5f3de9f4855ca9936e05ce24fca41abcec5988f
Julien Goudard <115709679+julienGdrd@users.noreply.github.com>
2024-02-06 14:28:49 +0100

    SetUp : jest ready to be used (#4)
    Co-authored-by: Julien Goudard <julien@wecount.io>

---
+++ frontend/src/__tests__/title.test.tsx
@@ -0,0 +1,18 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Title from "@/components/example/Title";
+
+describe("Title", () => {
+  it("renders title component", () => {
+    render(<Title />);
+    const heading = screen.getByRole("heading", { level: 3 });
+    expect(heading).toBeInTheDocument();
+    expect(heading.textContent).toBe("testTitre")
+  });
+
+  it("render the good content", ()=> {
+    render(<Title/>);
+    const content = screen.getByText("testTitre");
+    expect(content).toBeInTheDocument()
+  })
+});
---
+++ frontend/src/components/example/Title.tsx
@@ -0,0 +1,6 @@
+
+export default function Title(){
+    return(
+        <h3>testTitre</h3>
+    )
+}
