cbed9debd91f0e4e584a45d7bc1aec8176db438e
Eric_Gdf <79082962+Ericgdf@users.noreply.github.com>
2024-02-09 11:59:29 +0100

    mock_test_fonctionnel (#15)

---
+++ frontend/src/__tests__/mockTest/Book.test.tsx
@@ -0,0 +1,59 @@
+import '@testing-library/jest-dom';
+import { render, screen, waitFor } from '@testing-library/react';
+import { MockedProvider, MockedResponse } from '@apollo/client/testing';
+import ListBooks from '@/components/example/Book';
+import { Book } from '@/graphql/generated/schema';
+import GET_BOOKS from '@/graphql/queries-example/book.queries';
+
+const BookMock: MockedResponse<{ tags: Book[] }>[] = [
+  {
+    request: {
+      query: GET_BOOKS,
+    },
+    result: {
+      data: {
+        tags: [
+          {
+            id: 1,
+            title: 'Je suis pas trés lecture d`erreur TypeScript',
+            author: 'Julien',
+          },
+          { id: 2, title: 'Le petit Prince', author: 'Saint-EX' },
+        ],
+      },
+    },
+  },
+];
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+describe('Liste des livres', () => {
+  it('récupération de liste et affichage des éléments', async () => {
+    render(
+      <MockedProvider mocks={BookMock} addTypename={false}>
+        <ListBooks />
+      </MockedProvider>,
+    );
+    await waitFor(() => {
+      expect(
+        screen.getByText('Je suis pas trés lecture d`erreur TypeScript'),
+      ).toBeInTheDocument();
+    });
+  });
+});
+
+describe('Liste des livres', () => {
+  it('récupération de liste et affichage des éléments', async () => {
+    const container = render(
+      <MockedProvider mocks={BookMock} addTypename={false}>
+        <ListBooks />
+      </MockedProvider>,
+    );
+    await waitFor(() => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      expect(container).toMatchSnapshot();
+    });
+  });
+});
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import { useGetBooksQuery } from '@/graphql/generated/schema';
 import Title from '@/components/example/Title';
+import ListBooks from '@/components/example/Book';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -26,6 +27,7 @@ export default function Home() {
           <code className='font-mono font-bold'>src/pages/index.tsx</code>
         </p>
         <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'></div>
+        <ListBooks />
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]"></div>
